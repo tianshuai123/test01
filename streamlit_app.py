bfd3fb8772f24365857cf4036a10a576de8aca91
tianshuai123 <109144612+tianshuai123@users.noreply.github.com>
2023-05-12 18:10:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import streamlit as st
 from datetime import time, datetime
 
@@ -8,3 +10,9 @@ st.subheader('Slider')
 
 age = st.slider('How old are you?', 0, 130, 25)
 st.write(f"I'm {age} years old")
+
+
+st.subheader('Range slider')
+
+values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
+st.write('Values: ', values)
